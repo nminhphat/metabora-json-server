d5e1a820a4f606acf80002835554d9255652fdd6
nminhphat <nminhphat@tma.com.vn>
2023-02-28 15:54:07 +0700

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,15 @@
 const jsonServer = require("json-server");
 const server = jsonServer.create();
+
+server.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.header("Access-Control-Allow-Headers", "*");
+  next();
+});
+
 const router = jsonServer.router("db_mock_data.json");
 const middlewares = jsonServer.defaults();
+
 const port = process.env.PORT || 3000;
 
 server.use(middlewares);
